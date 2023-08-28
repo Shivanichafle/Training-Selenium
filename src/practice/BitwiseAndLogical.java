2e5c653336870e1e3e73cb03eb4fde31cb0b0a57
HP <HP@LAPTOP-J0LKEI75>
2023-08-28 15:12:43 +0530

    This program is for bitwise and logical operator.

---
+++ src/practice/BitwiseAndLogical.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class BitwiseAndLogical {
+
+	public static void main(String[] args) {
+		
+		System.out.println( returnFalse() && returnFalse());
+	}
+
+	public static boolean returnTrue() {
+		System.out.println("executing returnTrue");
+		return true;
+	}
+
+	public static boolean returnFalse() {
+		System.out.println("executing returnFalse");
+		return false;
+		
+
+	}
+
+}
