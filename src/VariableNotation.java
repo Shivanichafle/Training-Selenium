264a90d1fe5014c67f3eec264d2584f42d42a193
HP <HP@LAPTOP-J0LKEI75>
2023-10-03 19:29:18 +0530

    Variable notation Program

---
+++ src/VariableNotation.java
@@ -0,0 +1,20 @@
+
+public class VariableNotation {
+
+	public static void main(String[] args) {
+		int i = 10; 
+
+		//int int = 20; 
+
+		//int goto = 30; 
+
+		//int default = 40; 
+
+		//int class = 50; 
+
+		int j = 70;
+		System.out.println(i);
+		System.out.println(j);
+	}
+
+}
